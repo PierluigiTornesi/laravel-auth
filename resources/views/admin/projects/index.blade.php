2a19fba218566afcee8a40d585991e38eb854957
PierluigiTornesi <pierluigi.tornesi02@gmail.com>
2024-01-23 15:27:35 +0100

    add: lista projects

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container mt-5">
+        <h2>Lista dei progetti</h2>
+
+        <table class="table table-striped mt-5">
+            <thead>
+              <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Titolo</th>
+                <th scope="col">Azioni</th>
+              </tr>
+            </thead>
+            <tbody>
+                @foreach ($projects as $project)
+                    <tr>
+                    <th scope="row">{{$project->id}}</th>
+                    <td>{{$project->title}}</td>
+                    <td></td>
+                  </tr>
+                @endforeach 
+            </tbody>
+          </table>
+    </div>
+@endsection
