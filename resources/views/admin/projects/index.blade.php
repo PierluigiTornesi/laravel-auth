70535fceb6289e14be23c17ef23a9bd018af73d3
PierluigiTornesi <pierluigi.tornesi02@gmail.com>
2024-01-24 16:34:12 +0100

    add: delete

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -8,6 +8,12 @@
             <a class="btn btn-success" href="{{ route('admin.projects.create')}}">Create a new project</a>
         </div>
 
+        @if (session('message'))
+            <div class="alert alert-success">
+              {{ session('message')}}
+            </div>
+        @endif
+
         @if (count($projects) > 0)
           <table class="table table-striped mt-5">
             <thead>
@@ -25,6 +31,13 @@
                     <td>
                       <a class="btn btn-success" href="{{route('admin.projects.show', ['project' => $project->slug]) }}">Details</a>
                       <a class="btn btn-warning" href="{{route('admin.projects.edit', ['project' => $project->slug]) }}">Edit</a>
+
+                      <form class="d-inline-block" action="{{ route('admin.projects.destroy' , [ 'project' => $project->slug])}}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        
+                        <button class="btn btn-danger" type="submit">Delete</button>
+                      </form>
                     </td>
                   </tr>
                 @endforeach 
